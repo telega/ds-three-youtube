0e8042b75e370393f298f7df0abd465bd0bb97d3
telega <tom@telega.org>
2019-07-25 21:46:11 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Require  html-webpack-plugin plugin
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: __dirname + "/src/app/app.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + "/dist", // Folder to store generated bundle
+    filename: "bundle.js", // Name of generated bundle after build
+    publicPath: "/" // public URL of the output directory when referenced in a browser
+  },
+  module: {
+    // where we defined file patterns and their loaders
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              //publicPath: "../",
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: "body"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    })
+  ],
+  devServer: {
+    // configuration for webpack-dev-server
+    contentBase: "./src/public", //source of static assets
+    port: 7700 // port to run dev-server
+  }
+};
